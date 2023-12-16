c794eae784889eda596a1e681c0ca294cc5c74ed
Amadeusz Drozdz <adrozdz@softserveinc.com>
2023-12-16 07:54:16 +0100

    py

--- _playground/5.py
+++ _playground/5.py
@@ -8,7 +8,7 @@ class ListNode:
 
 
 def has_cycle(head: ListNode) -> bool:
-    if head is None:
+    if not head:
         return False
 
     slow = fast = head
@@ -23,8 +23,8 @@ def has_cycle(head: ListNode) -> bool:
     return False
 
 
-def get_intersection(head: ListNode) -> Optional[ListNode]:
-    if head is None:
+def find_insect(head: ListNode) -> Optional[ListNode]:
+    if not head:
         return
 
     slow = fast = head
@@ -35,8 +35,18 @@ def get_intersection(head: ListNode) -> Optional[ListNode]:
 
         if slow == fast:
             return slow
-
     return
+
+
+def find_first_insect(head: ListNode, insect: ListNode) -> Optional[ListNode]:
+    if not head:
+        return
+
+    while head != insect:
+        head = head.next
+        insect = insect.next
+
+    return head
 
 
 ########################################################################
@@ -44,7 +54,7 @@ def get_intersection(head: ListNode) -> Optional[ListNode]:
 #  When both the slow and fast pointers meet at the same node,         #
 #  it indicates the presence of a cycle in the list.                   #
 ########################################################################
-#                                                                      #                    
+#                                                                      #
 #  1. Array representation of cycle                                    #
 #     Value points to index in array                                   #
 #                                                                      #
@@ -57,10 +67,10 @@ def get_intersection(head: ListNode) -> Optional[ListNode]:
 #                 ↑                                                    #
 #                 ⎩_  Multiple nodes point to a single node,           #
 #                     proving that it is a cyclic list.                #
-#                                                                      # 
+#                                                                      #
 #                                                                      #
 #   2. Linked List representation of a cycle:                          #
-#                                                                      # 
+#                                                                      #
 #                        (3)                                           #
 #                       ↗️   ↘️                                          #
 #      head:     (1) ➡️ (2)     〉                                      #
@@ -69,9 +79,8 @@ def get_intersection(head: ListNode) -> Optional[ListNode]:
 #                     |                                                #
 #                     ⎩_ Multiple nodes point to a single node,        #
 #                        proving that it is a cyclic list.             #
-#                                                                      # 
+#                                                                      #
 ########################################################################
-
 
 
 n2, n3, n4 = ListNode(2), ListNode(3), ListNode(4)
@@ -85,5 +94,7 @@ output = "No Cycle Detected" if not res else "Cycle Detected"
 print(output)
 
 
-res = get_intersection(head1)
-print(res.value if res else "No Cycle")
+insect = find_insect(head1)
+
+res = find_first_insect(head1, insect)
+print(res.value if res else "No first isect")
