1acf857add8b56bdb9899f0ea2dd322c33975e27
Amadeusz Drozdz <adrozdz@softserveinc.com>
2023-12-19 01:00:02 +0100

    90

--- Backtracking/90 Subsets II.py
+++ Backtracking/90 Subsets II.py
@@ -11,16 +11,14 @@ class Solution:
             if len(path) == len(nums):
                 return res
 
-            seen = set()  # Track elements added at this level, so only distinct numbers will be checked
+            seen = set()  # Only distinct numbers will be checked
             for i, n in enumerate(nums):
+
                 if n < minN or n in seen or vis[i]:
                     continue
 
                 vis[i] = True
-
-                r = backtrack(path + [n], vis, n)
-                res.extend(r)
-
+                res.extend(backtrack(path + [n], vis, n))
                 vis[i] = False
 
                 seen.add(n)
@@ -30,34 +28,43 @@ class Solution:
         visited = [False for _ in nums]
         return backtrack([], visited, -math.inf)
 
-# Better sol
+
+sol = Solution()
+res = sol.subsetsWithDup([2, 1, 2])
+print(res)
+
+
+# A more elegant solution where the visited array is replaced by a start index array.
+# Unfortunately, it doesn't work correctly in all cases - Works for sorted input!
+#
+#     Input                       Result
+#   [1, 2, 2] -> [[], [1], [2], [1, 2], [2, 2], [1, 2, 2]]
+#   [2, 1, 2] -> [[], [1], [2], [1, 2], [2, 2]           ]
+#
+# This approach fails to return the last path, since the array is reduced from the start
+# and the last value is at index 0. Input array would have to be sorted first.
+
+
 # class Solution:
 #     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
 
-#         def backtrack(path: List[int], start: int):
+#         def backtrack(path: List[int], start: int, minN: int):
 #             res = [path]
 
 #             if len(path) == len(nums):
 #                 return res
 
-#             seen = set()  # Track elements added at this level, so only distinct numbers will be checked
+#             seen = set()  # Only distinct numbers will be checked
 #             for i in range(start, len(nums)):
 #                 n = nums[i]
 
-#                 if n in seen:
+#                 if n < minN or n in seen:
 #                     continue
 
-#                 r = backtrack(path + [n], i + 1)
-#                 res.extend(r)
+#                 res.extend(backtrack(path + [n], i + 1, n))
 
 #                 seen.add(n)
 
 #             return res
 
-#         return backtrack([], 0)
-
-
-
-sol = Solution()
-res = sol.subsetsWithDup([1, 2, 2])
-print(res)
+#         return backtrack([], 0, -math.inf)
