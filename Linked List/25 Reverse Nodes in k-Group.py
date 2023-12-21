7fc18b4d5824a4d01f843f63abf207f3f0fed2bf
Amadeusz Drozdz <adrozdz@softserveinc.com>
2023-12-21 01:31:15 +0100

    25

---
+++ Linked List/25 Reverse Nodes in k-Group.py
@@ -0,0 +1,71 @@
+from typing import Deque, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if k <= 1:
+            return head
+
+
+        isH = lambda i: i % k == 0  # True if n is group head
+        isT = lambda i: i % k == k - 1  # True if n is group tail
+        isS = lambda i: i + 1 == k  # True if n is list head
+
+        q = Deque()
+        c, p, i = head, None, 0
+        while c:
+            if isH(i):
+                q.append(c)  # Store group head
+                p = None
+            elif isS(i):
+                head = c  # Set head to list start
+            elif isT(i):
+                q.popleft().next = c  # Retrieve group head
+
+            # Reverse
+            tmp = c.next
+            c.next = p
+            p = c
+            c = tmp
+
+            i += 1
+
+        f = q.popleft()
+        l = q.popleft() if q else None
+
+        f.next = l
+
+        if not l:
+            return head
+
+        c, p = p, None
+        while c:
+            tmp = c.next
+            c.next = p
+            p = c
+            c = tmp
+
+        return head
+
+
+def printList(head: ListNode):
+    while head:
+        print(head.val, end=" ")
+        head = head.next
+    print()
+
+
+l = ListNode(val=1,
+             next=ListNode(val=2,
+                           next=ListNode(val=3,
+                                         next=ListNode(val=4,
+                                                       next=ListNode(5)))))
+
+sol = Solution().reverseKGroup(l, 3)
+printList(sol)
--- _playground/10.py
+++ _playground/10.py
@@ -1,1 +1,42 @@
-print(5 // 2)
+
+
+from typing import Deque
+
+isH = lambda i, k: not i % k
+isT = lambda i, k: i % k == k - 1
+isS = lambda i, k: i == k - 1
+
+k = 2
+
+# print(isH(0, k))
+# print(isH(1, k))
+# print(isH(2, k))
+# print(isH(3, k))
+# print(isH(4, k))
+# print(isH(5, k))
+# print(isH(6, k))
+# print(isH(7, k))
+
+# print()
+
+# print(isT(0, k))
+# print(isT(1, k))
+# print(isT(2, k))
+# print(isT(3, k))
+# print(isT(4, k))
+# print(isT(5, k))
+# print(isT(6, k))
+# print(isT(7, k))
+
+
+i = 1
+
+# Store head
+if isH(i, k):
+    print("isH")
+
+elif isS(i, k):
+    print("isS")
+
+elif isT(i, k):
+    print("isT")
