2ffc8f6aaa753043000bb4df4c19edcd27154c66
Amadeusz Drozdz <adrozdz@softserveinc.com>
2024-01-16 19:02:36 +0100

    417

--- Graphs/417 Pacific Atlantic Water Flow.py
+++ Graphs/417 Pacific Atlantic Water Flow.py
@@ -1,4 +1,4 @@
-from typing import Deque, List
+from typing import Deque, List, Set
 
 
 # class Solution:
@@ -57,43 +57,58 @@ class Solution:
 
         direct = ((-1, 0), (0, -1), (1, 0), (0, 1))
 
-        def dfs(row: int, col: int):
-            pacific, atlantic = False, False
-
-            vis = set([(row, col)])
-            q = Deque([(row, col)])
+        def dfs(q: Deque, vis: Set):
+            res = []
 
             while q:
                 row, col = q.pop()
-
+                res.append((row, col))
                 for dr, dc in direct:
                     r, c = row + dr, col + dc
-
-                    if r < 0 or c < 0:
-                        pacific = True
-                    elif r >= ROWS or c >= COLS:
-                        atlantic = True
-
-                    if pacific and atlantic:
-                        return True
 
                     if (
                         r not in range(ROWS)
                         or c not in range(COLS)
                         or (r, c) in vis
-                        or heights[r][c] > heights[row][col]
+                        or heights[row][col] > heights[r][c]
                     ):
                         continue
 
                     q.append((r, c))
                     vis.add((r, c))
+            return res
 
-            return pacific and atlantic
+        q, vis = Deque(), set()
+
+        for r in range(ROWS):
+            q.append((r, 0))
+            vis.add((r, 0))
+
+        for c in range(COLS):
+            if c != 0:
+                q.append((0, c))
+                vis.add((0, c))
+
+        pacific = dfs(q, vis)
+
+        q, vis = Deque(), set()
+
+        for r in range(ROWS):
+            q.append((r, COLS - 1))
+            vis.add((r, COLS - 1))
+
+        for c in range(COLS):
+            if c != COLS:
+                q.append((ROWS - 1, c))
+                vis.add((ROWS -1, c))
+
+        arctic = dfs(q, vis)
+
 
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r, c):
-                    res.append([r, c])
+                if (r,c) in pacific and (r,c) in arctic:
+                    res.append([r,c])
 
         return res
 
