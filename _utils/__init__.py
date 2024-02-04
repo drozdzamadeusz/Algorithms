e1201a8c5d5c7d55d88092e49537f7f1d8c407c5
Amadeusz Drozdz <adrozdz@softserveinc.com>
2024-02-04 16:54:40 +0100

    test improvment

--- _utils/__init__.py
+++ _utils/__init__.py
@@ -20,23 +20,23 @@ Color = Literal[
 ]
 
 
-def equal(output, expected=None) -> bool:
+def equal(output, expected=None, res_prefix="") -> bool:
     if output != expected and not expected:
-        print(Text.formatHeader(ResultType.INFO))
+        print(Text.formatHeader(ResultType.INFO, res_prefix))
         Text.printWithGap(output)
         return True
 
     res = output == expected
 
     if res:
-        print(Text.formatHeader(ResultType.PASSED))
+        print(Text.formatHeader(ResultType.PASSED, res_prefix))
     else:
-        print(Text.formatHeader(ResultType.FAILED))
+        print(Text.formatHeader(ResultType.FAILED, res_prefix))
         Text.printWithGap(Text.color(Text.bold("Result: "), 'white'))
         Text.printWithGap(f'{output}\n')
 
     Text.printWithGap(Text.color(Text.bold("Expected: "), 'white'))
-    Text.printWithGap(expected)
+    Text.printWithGap(f'{expected}\n')
 
     return res
 
@@ -58,7 +58,7 @@ class Text:
         return colored(text, color)
 
     @staticmethod
-    def formatHeader(type, text="", prefix=""):
+    def formatHeader(type, prefix, text=""):
         return Text.results[type](text, prefix)
 
     @staticmethod
