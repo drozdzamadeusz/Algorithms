7cc7fb9ea0d0617cbaf4d29126e04980e19b8c3c
Amadeusz Drozdz <adrozdz@softserveinc.com>
2024-02-04 16:54:48 +0100

    198

--- DP/198 House Robber.py
+++ DP/198 House Robber.py
@@ -5,6 +5,12 @@ from _utils import equal
 class Solution:
     def rob(self, nums: List[int]) -> int:
         LEN = len(nums)
+
+        if LEN == 2:
+            return max(nums[0], nums[1])
+        elif LEN == 1:
+            return nums[0]
+
         dp = [0] * LEN
 
         dp[0] = nums[0]
@@ -12,13 +18,14 @@ class Solution:
         dp[2] = nums[2] + nums[0]
 
         for i in range(3, LEN):
-            i_2 = dp[i - 2]
             i_3 = dp[i - 3]
-            max_prev = max(i_2, i_3)
-            dp[i] = max_prev + nums[i]
+            i_2 = dp[i - 2]
+            # i - 1 is skiped
+            dp[i] = max(i_3, i_2) + nums[i]
 
         return max(dp[LEN - 1], dp[LEN - 2])
 
 
 sol = Solution()
-equal(sol.rob([1, 2, 7, 2, 1, 9]), 17)
+equal(sol.rob([1, 2, 3, 1]), 4)
+equal(sol.rob([2, 7, 9, 3, 1]), 12)
