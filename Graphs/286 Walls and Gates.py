413322dc97f5121e565f4feb3eee01086c5bc26c
Amadeusz Drozdz <adrozdz@softserveinc.com>
2024-02-06 20:04:31 +0100

    walls and gates

--- Graphs/286 Walls and Gates.py
+++ Graphs/286 Walls and Gates.py
@@ -35,3 +35,21 @@ class Solution:
                     explore(r, c)
         return rooms
 
+
+if __name__ == '__main__':
+    fun = Solution().wallsAndGates
+    test = Test(fun)
+    expected = [[3, -1, 0, 1], [2, 2, 1, -1], [1, -1, 2, -1], [0, -1, 3, 4]]
+    arg = [
+        [2147483647, -1, 0, 2147483647],
+        [2147483647, 2147483647, 2147483647, -1],
+        [2147483647, -1, 2147483647, -1],
+        [0, -1, 2147483647, 2147483647],
+    ]
+    test.equal(expected, arg)
+
+    arg = [[-1]]
+    expected = [[-1]]
+    test.equal(arg, expected)
+
+    test.run()
