59b7a7014c0ef584dbf63ce3d8d9663275b92efc
Amadeusz Drożdż <drozdz.amadeusz@gmail.com>
2024-06-30 20:57:40 +0200

    283

---
+++ Arrays/283 Move Zeroes.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        LEN = len(nums)
+        l = r = 0
+
+        while r < LEN:
+
+            if l != r:
+                nums[l] = nums[r]
+
+            if nums[l] != 0:
+                l += 1
+
+            r += 1
+
+        for i in range(LEN - (r - l), LEN):
+            nums[i] = 0
+
+        print(nums)
+
+
+sol = Solution()
+sol.moveZeroes([-58305, -22022, 0, 0, 0, 0, 0, -76070, 42820,
+               48119, 0, 95708, -91393, 60044, 0, -34562, 0, -88974])
