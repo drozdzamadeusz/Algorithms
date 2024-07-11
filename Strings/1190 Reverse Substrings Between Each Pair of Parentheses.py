433b5c8f837914216b87556f4b61edfdbf0c5008
Amadeusz Drozdz <adrozdz@softserveinc.com>
2024-07-11 13:52:36 +0200

    1190

--- Strings/1190 Reverse Substrings Between Each Pair of Parentheses.py
+++ Strings/1190 Reverse Substrings Between Each Pair of Parentheses.py
@@ -11,7 +11,7 @@ class Solution:
                 o_idx = opens_idx.pop()
                 sub_s = s[o_idx + 1:i][::-1]
                 s = s[:o_idx] + sub_s + s[i + 1:]
-                i -= 2 # parentheses pair is removed, so 2 characters are removed form s after current index, thus decrement index by 2
+                i -= 2  # parentheses pair is removed, so 2 characters are removed form s after current index, thus decrement index by 2
             i += 1
 
         return s
